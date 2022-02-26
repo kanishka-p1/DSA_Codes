695ef232ac2b6f0ca4d019a9209adf289dff9c54
kaystion <92727668+kanishka-p1@users.noreply.github.com>
2022-02-26 12:39:23 +0530

    Add files via upload

---
+++ zigzag.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void pzz(int n)
+{
+    if (n == 1)
+    {
+        cout << "111";
+        return;
+    }
+    cout << n;
+    pzz(n - 1);
+    cout << n;
+    pzz(n - 1);
+    cout << n;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    pzz(n);
+}
