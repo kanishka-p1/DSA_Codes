b402adb8aa897297bf11287049d4f57867e49f29
kaystion <92727668+kanishka-p1@users.noreply.github.com>
2022-06-06 20:15:04 +0530

    Create print_subset_sum.cpp

---
+++ subset_sum/print_subset_sum.cpp
@@ -0,0 +1,32 @@
+void subsetSumToKHelper(int input[], int startIndex, int size, int output[], int oSize, int k) {
+    if(startIndex == size) 
+    { 
+        if(k == 0) 
+        {
+            for(int i = 0; i < oSize; i++) 
+            {
+                cout << output[i] << " ";
+            }
+            cout << endl;
+        }
+        return;
+    }
+    int smallOutout1[1000], smallOutput2[1000], smallSize1 = 0, smallSize2 = 0;
+    subsetSumToKHelper(input, startIndex+1, size, output, oSize, k);
+    if(k > 0) 
+    {
+        int i;
+        for(i = 0; i < oSize; i++) 
+        {
+            smallOutput2[i] = output[i];
+        }
+        smallOutput2[i] = input[startIndex];
+        subsetSumToKHelper(input, startIndex+1, size, smallOutput2, oSize+1, k-input[startIndex]);
+    }
+}
+
+void printSubsetSumToK(int input[], int size, int k) {
+    // Write your code here
+    int output[1000]; 
+    subsetSumToKHelper(input, 0, size, output, 0, k);
+}
