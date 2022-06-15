9707893f70d6b702f6e6c15783c326679c77873c
kaystion <92727668+kanishka-p1@users.noreply.github.com>
2022-06-15 16:01:03 +0530

    Update construct-tree(postorder-and-inorder).cpp

--- Binary Trees/Construct Tree/construct-tree(postorder-and-inorder).cpp
+++ Binary Trees/Construct Tree/construct-tree(postorder-and-inorder).cpp
@@ -22,3 +22,39 @@ BinaryTreeNode<int>* buildTree(int *postorder, int postLength, int *inorder, int
     
     return root;
 }
+
+            *********************************************************************************************************************************************
+            
+BinaryTreeNode<int>* buildTreeHelper(int* in, int* post, int is, int ie, int ps, int pe) {
+    if(is > ie) {
+        return NULL;
+    }
+    
+    int rootdata = post[pe];
+    int rootindex = -1;
+    for(int i = is; i <= ie; i++) {
+        if(in[i] == rootdata) {
+            rootindex = i;
+            break;
+        }
+    }
+    int lins = is;
+    int line = rootindex - 1;
+    int lposts = ps;
+    int lposte = rootindex - 1 - is + ps;
+    int rins = rootindex + 1;
+    int rine = ie;
+    int rposts = ps + rootindex - is;
+    int rposte =  pe - 1;
+    
+    BinaryTreeNode<int>* root = new BinaryTreeNode<int>(rootdata);
+    root->left = buildTreeHelper(in, post, lins, line, lposts, lposte);
+    root->right = buildTreeHelper(in, post, rins, rine, rposts, rposte);
+  return root;
+}
+
+
+BinaryTreeNode<int>* buildTree(int *postorder, int postLength, int *inorder, int inLength) {
+    // Write your code here
+    return buildTreeHelper(inorder, postorder, 0, postLength - 1, 0, postLength - 1);
+}
