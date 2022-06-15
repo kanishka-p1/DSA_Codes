0a242f58b87939f0a84775f02a2f939cb2f12457
kaystion <92727668+kanishka-p1@users.noreply.github.com>
2022-06-15 19:24:30 +0530

    Update diameter-binary-tree.cpp

--- Binary Trees/diameter-binary-tree.cpp
+++ Binary Trees/diameter-binary-tree.cpp
@@ -20,3 +20,29 @@ int diameter(BinaryTreeNode<int>* root) {
   
   return max(a, max(b, c));
 }
+
+             *************************************************************************************************************************************
+            
+pair<int, int> heightDiameter(BinaryTreeNode<int>* root) {
+  if(root == NULL) {
+      pair<int, int> p;
+      p.first = 0;
+      p.second = 0;
+      return p;
+  }
+                 
+  pair<int, int> lans = heightDiameter(root->left);
+  pair<int, int> rans = heightDiameter(root->right);                  
+  int lh = lans.first;
+  int ld = lans.second;
+  int rh = rans.first;
+  int rd = rans.second;
+  int hans = 1 + max(lh, rh);
+  int dans = max(lh + rh, max(ld, rd));
+                 
+  pair<int, int> ans;
+  p.first = hans;
+  p.second = dans;
+                 
+  return p;
+}
