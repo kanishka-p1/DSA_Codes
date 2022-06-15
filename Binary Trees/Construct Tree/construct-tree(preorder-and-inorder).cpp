fc554f052d9813c3907051388c47420c3958cbd0
kaystion <92727668+kanishka-p1@users.noreply.github.com>
2022-06-15 15:36:06 +0530

    Update construct-tree(preorder-and-inorder).cpp

--- Binary Trees/Construct Tree/construct-tree(preorder-and-inorder).cpp
+++ Binary Trees/Construct Tree/construct-tree(preorder-and-inorder).cpp
@@ -25,3 +25,37 @@ BinaryTreeNode<int>* buildTree(int *preorder, int preLength, int *inorder, int i
     
     return root;
 }
+
+    *************************************************************************************************************************************************************
+
+BinaryTreeNode<int>* buildTreeHelper(int *in, int* pre, int is, int ie, int ps, int pe) {
+  if(is > ie) {
+    return NULL;
+  }
+  
+  int rootdata = pre[ps];
+  int rootindex = -1;
+  for(int i = is; i <= ie; i++) {
+    if(in[i] == rootdata) {
+      rootindex = i; break;
+    }
+  }
+  int lins = is;
+  int line = rootindex - 1;
+  int lpres = ps + 1;
+  int lpree = line - lins + lpres;
+  int rpres = lpree + 1;
+  int rpree = pe;
+  int rins = rootindex + 1;
+  int rine = ie;
+  
+  BinaryTreeNode<int>* root = new BinaryTreeNode<int>(rootdata);
+  root->left = buildTreeHelper(in, pre, lins, line, lpres, lpree);
+  root->right = buildTreeHelper(in, pre, rins, rine, rpres, rpree);
+  return root;
+}
+
+
+BinaryTreeNode<int>* buildTree(int *preorder, int preLength, int *inorder, int inLength) {
+  return buildTreeHelper(inorder, preorder, 0, preLength - 1, 0, preLength - 1);
+}
