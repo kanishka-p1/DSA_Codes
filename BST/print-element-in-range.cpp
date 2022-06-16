cfe0a4c0aae44606e0589bfe9faac3ac5477aa2f
kaystion <92727668+kanishka-p1@users.noreply.github.com>
2022-06-16 14:55:21 +0530

    Create print-element-in-range.cpp

---
+++ BST/print-element-in-range.cpp
@@ -0,0 +1,16 @@
+void elementsInRangeK1K2(BinaryTreeNode<int>* root, int k1, int k2) {
+	// Write your code here
+    if(root == NULL){
+        return;
+    }
+    
+    if(k1 <= root -> data){
+        elementsInRangeK1K2(root -> left, k1, k2);
+    }
+    if(k1 <= root -> data && root -> data <= k2){
+        cout << root -> data << " ";
+    }
+    if(k2 >= root -> data){
+        elementsInRangeK1K2(root -> right, k1, k2);
+    }
+}
