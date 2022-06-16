99640950e7bb9fdb15f67ffb94960a5863a5a331
kaystion <92727668+kanishka-p1@users.noreply.github.com>
2022-06-16 18:45:37 +0530

    Create path-in-BST.cpp

---
+++ BST/path-in-BST.cpp
@@ -0,0 +1,28 @@
+#include<vector>
+vector<int>* getPath(BinaryTreeNode<int> *root , int data) {
+	// Write your code here
+    if(root == NULL){
+        return NULL;
+    }
+    if(root -> data == data){
+        vector<int> *output = new vector<int>();
+        output -> push_back(root -> data);
+        return output;
+    }
+    
+    vector<int> *leftOutput = getPath(root -> left, data);
+    if(leftOutput != NULL){
+        leftOutput -> push_back(root -> data);
+        return leftOutput;
+    }
+    else{
+        vector<int> *rightOutput = getPath(root -> right, data);
+        if(rightOutput != NULL){
+            rightOutput -> push_back(root -> data);
+            return rightOutput;
+        }
+        else{
+            return NULL;
+        }
+    }
+}
