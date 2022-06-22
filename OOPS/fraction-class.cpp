f3f4dff597294ca504acb98366353461a2fc7622
kaystion <92727668+kanishka-p1@users.noreply.github.com>
2022-06-22 17:18:21 +0530

    Update fraction-class.cpp

--- OOPS/fraction-class.cpp
+++ OOPS/fraction-class.cpp
@@ -46,16 +46,40 @@ class Fraction {
     den = den / gcd;
   }
   
-  void add(Fraction const &f2) {
+  Fraction add(Fraction const &f2) {
     int lcm = den * f2.den;
     int x = lcm / den;
     int y = lcm / f2.den;
     int numerator = x * num + (y * f2.num);
     
-    num = numerator;
-    den = lcm;
+    Fraction fnew(numerator, lcm);
+    fnew.simplify();
     
-    simplify();
+    return fnew;
+  }
+  
+    Fraction operator+(Fraction const &f2) const {
+    int lcm = den * f2.den;
+    int x = lcm / den;
+    int y = lcm / f2.den;
+    int numerator = x * num + (y * f2.num);
+    
+    Fraction fnew(numerator, lcm);
+    fnew.simplify();
+    
+    return fnew;
+  }
+  
+  Fraction operator*(Fraction const &f2) const {
+    int n = num * f2.num;
+    int d = den * f2.den;
+    Fraction fnew(n, d);
+    fnew.simplify();
+    return fnew;
+  }
+  
+  bool operator==(Fraction const &f2) const {
+    return (num == f2.num && den == f2.den);
   }
   
   void multiply(Fraction const &f2) {
@@ -71,13 +95,27 @@ int main() {
   Fraction f1(10, 2); 
   Fraction f2(15, 4);
   
-  f1.add(f2);
+  //f1.add(f2);
+  //f1.print();
+  //f2.print();
+  //f1.multipy(f2);
+  //f1.print();
+  //f2.print();
   
+  Fraction f3 = f1.add(f2);
+  Fraction f4 = f1 + f2;
   f1.print();
   f2.print();
+  f3.print();
+  f4.print();
   
-  f1.multipy(f2);
+  Fraction f5 = f1 * f2;
+  f5.print();
   
-  f1.print();
-  f2.print();
+  if(f1 == f2) {
+    cout << "equal" << endl;
+  }
+  else {
+    cout << "not equal" << endl;
+  }
 }
