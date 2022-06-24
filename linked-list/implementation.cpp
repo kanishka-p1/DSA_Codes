0051853b1c9ac281fa70c51281d7e73e42b32d29
kaystion <92727668+kanishka-p1@users.noreply.github.com>
2022-06-24 08:54:03 +0530

    Update implementation.cpp

--- linked-list/implementation.cpp
+++ linked-list/implementation.cpp
@@ -12,3 +12,38 @@ class Node {
 	}
 };
 
+void print(Node* head) {
+	Node* temp = head;
+	while(!temp != NULL) {
+		cout << temp->data << " ";
+		temp = temp->next;
+	}
+	cout << endl;
+}
+
+Node* input() {
+	int data;
+	cin >> data;
+	Node* head = NULL;
+	while(data != -1) {
+		Node* n = new Node(data);
+		if(head == NULL) {
+			head = n;
+		}
+		else {
+			Node* temp = head;
+			while(temp->next != NULL) {			
+				temp = temp->next;
+			}
+			temp->next = n;
+		}
+		cin >> data;
+	}
+	return head;
+}
+
+
+int main() {
+	Node* head = input();
+	print(head);	
+}
