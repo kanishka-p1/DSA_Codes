05383a7c3f629af9a5a0c9db76ca040dc8144b47
kaystion <92727668+kanishka-p1@users.noreply.github.com>
2022-07-02 22:12:51 +0530

    Update brute-knapsack.cpp

--- DP/Knapsack/brute-knapsack.cpp
+++ DP/Knapsack/brute-knapsack.cpp
@@ -11,3 +11,17 @@ int knapsack(int *weights, int *values, int n, int maxWeight)
         return max(values[0] + knapsack(weights + 1, values + 1, n - 1, maxWeight - weights[0]), knapsack(weights + 1, values + 1, n - 1, maxWeight));
     
 }
+
+//Approach 2
+int knapsack(int *weights, int *values, int n, int maxWeight) {
+	if(n == 0 || maxWeight == 0) {
+		return 0;
+	}
+	
+	if(weight[n - 1] <= maxWeight) {
+		return max(values[n - 1] + knapsack(weights, values, n - 1, maxWeight - weight[n - 1]), knapsack(weights, values, n - 1, maxWeight));
+	}
+	else {
+		return knapsack(weights, values, n - 1, maxWeight);
+	}
+}
