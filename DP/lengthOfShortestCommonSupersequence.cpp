e6182f0707c8e988b55c085867dd314d918fd2ae
Kanishka Panwar <92727668+kanishka-p1@users.noreply.github.com>
2022-07-25 18:09:56 +0530

    Create lengthOfShortestCommonSupersequence.cpp

---
+++ DP/lengthOfShortestCommonSupersequence.cpp
@@ -0,0 +1,31 @@
+int superSequence(string text1, string text2, int m, int n) {
+        int dp[m + 1][n + 1];
+        
+        for(int i = 0; i <= m ; i++) {
+            for(int j = 0; j <= n; j++) {
+                if(i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(text1[i - 1] == text2[j - 1]) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                }
+                else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        
+        return m + n + dp[m][n];
+    }
+    
+    int longestCommonSubsequence(string text1, string text2) {
+        int m = text1.length();
+        int n = text2.length();
+        
+        return superSequence(text1, text2, m, n);
+    }
