263a02e0a146a45beef9ff43b9676147da6ac11e
Kanishka Panwar <92727668+kanishka-p1@users.noreply.github.com>
2022-08-02 11:42:39 +0530

    Create maxMin.cpp

---
+++ InterviewBit/Arrays/maxMin.cpp
@@ -0,0 +1,61 @@
+//                                                                           QUESTION
+
+/*
+Given an array A of size N. You need to find the sum of Maximum and Minimum element in the given array.
+
+NOTE: You should make minimum number of comparisons.
+
+
+
+Problem Constraints
+1 <= N <= 105
+
+-109 <= A[i] <= 109
+
+
+
+Input Format
+First and only argument is an integer array A of size N.
+
+
+
+Output Format
+Return an integer denoting the sum Maximum and Minimum element in the given array.
+
+
+
+Example Input
+Input 1:
+
+ A = [-2, 1, -4, 5, 3]
+Input 2:
+
+ A = [1, 3, 4, 1]
+
+
+Example Output
+Output 1:
+
+ 1
+Output 2:
+
+ 5
+
+
+Example Explanation
+Explanation 1:
+
+ Maximum Element is 5 and Minimum element is -4. (5 + (-4)) = 1. 
+Explanation 2:
+
+ Maximum Element is 4 and Minimum element is 1. (4 + 1) = 5.
+ */
+
+
+//                                                                              ANSWER
+
+int Solution::solve(vector<int> &A) {
+    int n = A.size();
+    sort(A.begin(), A.end());
+    return A[0] + A[n - 1];
+}
