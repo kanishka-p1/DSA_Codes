25c0548dfbe1a2f4546d2369b3e5234a17fc6f3a
Kanishka Panwar <92727668+kanishka-p1@users.noreply.github.com>
2022-08-05 00:10:35 +0530

    Create reverseTheString.cpp

---
+++ InterviewBit/Strings/reverseTheString.cpp
@@ -0,0 +1,69 @@
+//                                                                    QUESTION
+
+/*
+Given a string A.
+
+Return the string A after reversing the string word by word.
+
+NOTE:
+
+A sequence of non-space characters constitutes a word.
+
+Your reversed string should not contain leading or trailing spaces, even if it is present in the input string.
+
+If there are multiple spaces between words, reduce them to a single space in the reversed string.
+
+
+ Input Format
+
+The only argument given is string A.
+Output Format
+
+Return the string A after reversing the string word by word.
+For Example
+
+Input 1:
+    A = "the sky is blue"
+Output 1:
+    "blue is sky the"
+
+Input 2:
+    A = "this is ib"
+Output 2:
+    "ib is this"
+
+Input 3:
+ A = " hello world "
+Output 3:
+ "world hello"
+*/
+
+
+//                                                                          ANSWER
+
+string Solution::solve(string A) {
+    reverse(A.begin(), A.end());
+    int l = 0, r = 0, i = 0, n = A.size();
+    
+    while(i < n) {
+        while(i < n && A[i] != ' ') {
+            A[r++] = A[i++];
+        }
+        if(l < r) {
+            reverse(A.begin() + l, A.begin() + r);
+            if(r == n) {
+                break;
+            }
+            A[r++] = ' ';
+            l = r;
+        }
+        i++;
+    }
+    
+    if(r > 0 && A[r - 1] == ' ') {
+        r--;
+    }
+    A.resize(r);
+    
+    return A;    
+}
