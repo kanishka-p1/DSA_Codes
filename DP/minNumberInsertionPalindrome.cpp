d92002612e0f0beb9f2a1d08e9c3d9c6b6f20de9
Kanishka Panwar <92727668+kanishka-p1@users.noreply.github.com>
2022-08-06 13:37:18 +0530

    Create minNumberInsertionPalindrome.cpp

---
+++ DP/minNumberInsertionPalindrome.cpp
@@ -0,0 +1,67 @@
+ //                                                                 QUESTION
+
+/*
+Given a string s. In one step you can insert any character at any index of the string.
+
+Return the minimum number of steps to make s palindrome.
+
+A Palindrome String is one that reads the same backward as well as forward.
+
+ 
+
+Example 1:
+
+Input: s = "zzazz"
+Output: 0
+Explanation: The string "zzazz" is already palindrome we don't need any insertions.
+Example 2:
+
+Input: s = "mbadm"
+Output: 2
+Explanation: String can be "mbdadbm" or "mdbabdm".
+Example 3:
+
+Input: s = "leetcode"
+Output: 5
+Explanation: Inserting 5 characters the string becomes "leetcodocteel".
+ 
+
+Constraints:
+
+1 <= s.length <= 500
+s consists of lowercase English letters.
+*/
+
+
+//                                                                              ANSWER
+
+
+class Solution {
+public:
+    int lcs(string a, string b, int m, int n) {
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(a[i - 1] == b[j - 1]) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                }
+                else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        
+        return dp[m][n];
+    }
+    
+    int minInsertions(string s) {
+        int n = s.length();
+        string str = s;
+        reverse(str.begin(), str.end());
+        
+        int ans = n - lcs(s, str, n, n);
+        
+        return ans;
+    }
+};
