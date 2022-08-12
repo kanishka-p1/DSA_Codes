68fcbcf581f10f886691fa9e099e719b4e822814
Kanishka Panwar <92727668+kanishka-p1@users.noreply.github.com>
2022-08-12 17:52:55 +0530

    Update matrixChainMultiplication.cpp

--- DP/matrixChainMultiplication.cpp
+++ DP/matrixChainMultiplication.cpp
@@ -73,3 +73,34 @@ public:
         return solve(arr, 1, N - 1);
     }
 };
+
+
+//Approach 2 (memoization)
+class Solution{
+public:
+    int solve(int arr[], int i, int j, vector<vector<int>>& dp) {
+        if(i >= j) {
+            return 0;
+        }
+        
+        if(dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        
+        int ans = INT_MAX;
+        
+        for(int k = i; k <= j - 1; k++) {
+            int temp = solve(arr, i, k, dp) + solve(arr, k + 1, j, dp) + (arr[i - 1] * arr[k] * arr[j]);
+            ans = min(ans, temp);
+        }
+        
+        return dp[i][j] = ans;
+    }
+
+    int matrixMultiplication(int N, int arr[])
+    {
+        // code here
+        vector<vector<int>> dp(N + 1, vector<int>(N + 1, -1));
+        return solve(arr, 1, N - 1, dp);
+    }
+};
