00c90e09cfbb8ce2c92dfc8b7db49cf151e2ddf9
Kanishka Panwar <92727668+kanishka-p1@users.noreply.github.com>
2022-09-03 14:19:00 +0530

    Create sudoku.cpp

---
+++ InterviewBit/Backtracking/sudoku.cpp
@@ -0,0 +1,81 @@
+//                                                                        QUESTION
+
+/*
+Write a program to solve a Sudoku puzzle by filling the empty cells.
+
+Empty cells are indicated by the character '.' 
+
+You may assume that there will be only one unique solution.
+
+
+
+A sudoku puzzle,
+
+
+
+and its solution numbers marked in red.
+
+Note: You must update the input argument A (partially completed grid of Sudoku) to submit your solved Sudoko grid.
+
+Example :
+
+For the above given diagrams, the corresponding input to your program will be
+
+[[53..7....], [6..195...], [.98....6.], [8...6...3], [4..8.3..1], [7...2...6], [.6....28.], [...419..5], [....8..79]]
+and we would expect your program to modify the above array of array of characters to
+
+[[534678912], [672195348], [198342567], [859761423], [426853791], [713924856], [961537284], [287419635], [345286179]]
+*/
+
+
+//                                                                          ANSWER
+
+bool isValid(vector<vector<char>>& board, int row, int col, char c) {
+    // row check
+    for(int i = 0; i < 9; i++) 
+		if(board[i][col] == c) 
+			return false;
+	// col check
+    for(int i = 0; i < 9; i++) 
+		if(board[row][i] == c) 
+			return false;
+    // box check
+    int x0 = (row/3) * 3, y0 = (col/3) * 3;
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
+            if(board[x0 + i][y0 + j] == c) return false;
+        }
+    }
+    return true;
+}
+
+bool helper(vector<vector<char>> &board, int row, int col) {
+    // done
+    if(row == 9) 
+		return true;
+    // time for next row
+    if(col == 9) 
+		return helper(board, row + 1, 0);
+    // already marked
+    if(board[row][col] != '.') 
+		return helper(board, row, col + 1);
+
+    for(char c = '1'; c <= '9'; c++) {
+        if(isValid(board, row, col, c)) {
+            board[row][col] = c;
+            // without return here, the board reverts to initial state
+            if(helper(board, row, col + 1))
+				return true;
+            board[row][col] = '.';
+        }
+    }
+    return false;
+}
+
+void Solution::solveSudoku(vector<vector<char> > &A) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+    helper(A, 0, 0);
+}
