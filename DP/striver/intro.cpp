ba5bc12b8179f8b9b1ea8e4f4d1e999f26a59160
Kanishka Panwar <92727668+kanishka-p1@users.noreply.github.com>
2022-12-11 15:26:56 +0530

    Update intro.cpp

--- DP/striver/intro.cpp
+++ DP/striver/intro.cpp
@@ -1,3 +1,6 @@
+/******************************************************************FIBONACCI***********************************************************************************/
+
+
 //{ Driver Code Starts
 // Initial Template for C++
 
@@ -53,3 +56,91 @@ int main() {
     }
 }
 // } Driver Code Ends
+
+
+/********************************************************************* CLIMBING STAIRS **********************************************************************/
+
+/* QUESTION */
+
+/*
+You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+ 
+
+Example 1:
+
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+Example 2:
+
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+ 
+
+Constraints:
+
+1 <= n <= 45
+*/
+
+
+/************************************************************************** RECURSION ***************************************************************************/
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n <= 2) {
+            return n;
+        }
+        return climbStairs(n - 1) + climbStairs(n - 2);
+    }
+};
+
+
+/*********************************************************************** MEMOIZATION *****************************************************************************/
+class Solution {
+public:
+    int climbStairs(int n) {
+         vector<int> dp(n + 1, -1);
+        if(n <= 2) {
+            dp[n] = n;
+            return n;
+        }
+        
+        if(dp[n] != -1) {
+            return dp[n];
+        }
+        
+        dp[n] = climbStairs(n - 1) + climbStairs(n - 2);
+        return dp[n];
+    }
+};
+
+
+/*************************************************************************** DP ************************************************************************************/
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> dp(n + 1, 0);
+        if(n <= 2) {
+            return n;
+        }
+        
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for(int i = 3; i < n + 1; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+};
