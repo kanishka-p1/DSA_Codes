dd4aa497257c2b9376e01d268b61ef6666d0a45a
Kanishka Panwar <92727668+kanishka-p1@users.noreply.github.com>
2022-12-17 11:13:16 +0530

    Update minimum_path_sum.cpp

--- DP/striver/minimum_path_sum.cpp
+++ DP/striver/minimum_path_sum.cpp
@@ -145,3 +145,27 @@ public:
         return dp[m - 1][n - 1];
     }
 };
+
+//approach 3
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        for(int i = 1; i < m; i++) {
+            grid[i][0] += grid[i -1][0];
+        }
+        for(int j = 1; j < n; j++) {
+            grid[0][j] += grid[0][j - 1];
+        }
+
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                grid[i][j] = grid[i][j] + min(grid[i - 1][j], grid[i][j - 1]);
+            }
+        }
+
+        return grid[m - 1][n - 1];
+    }
+};
