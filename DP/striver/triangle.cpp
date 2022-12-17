a3e1d367dd4ffd1707ad5c8ad2eabe5b8484dac7
Kanishka Panwar <92727668+kanishka-p1@users.noreply.github.com>
2022-12-17 20:44:11 +0530

    Update triangle.cpp

--- DP/striver/triangle.cpp
+++ DP/striver/triangle.cpp
@@ -89,4 +89,23 @@ public:
 
 /******************************************************************************* DP **********************************************************************************/
 
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int m = triangle.size();
 
+        vector<vector<int>> dp(m, vector<int>(m, -1));
+
+        for(int j = 0; j < m; j++) {
+            dp[m - 1][j] = triangle[m - 1][j];
+        }
+
+        for(int i = m - 2; i >= 0; i--) {
+            for(int j = i; j >= 0; j--) {
+                dp[i][j] = triangle[i][j] + min(dp[i + 1][j], dp[i + 1][j + 1]);
+            }
+        }
+        
+        return dp[0][0];
+    }
+};
