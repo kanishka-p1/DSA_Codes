887a6b3e865f00b41b621fd122ff04a340e92458
Kanishka Panwar <92727668+kanishka-p1@users.noreply.github.com>
2022-12-21 01:12:15 +0530

    Update count-of-subsets-with-a-given-sum.cpp

--- DP/count-of-subsets-with-a-given-sum.cpp
+++ DP/count-of-subsets-with-a-given-sum.cpp
@@ -85,7 +85,7 @@ int countSubset(vector<int>* arr, int n, int sum) {
     dp[0][j] = 0;
   }
   
-  for(int i = 0; i < n + 1; i++) {
+  for(int i = 1; i < n + 1; i++) {
     for(int j = 0; j < sum + 1; j++) {
       if(arr[i - 1] <= j) {
         dp[i][j] = dp[i - 1][j - arr[i - 1]] + dp[i - 1][j];
