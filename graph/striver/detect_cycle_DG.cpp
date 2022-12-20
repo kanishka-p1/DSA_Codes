3b243cdae39890f639cb03b121e4ff7a7dfa6a45
Kanishka Panwar <92727668+kanishka-p1@users.noreply.github.com>
2022-12-21 00:49:07 +0530

    Create detect_cycle_DG.cpp

---
+++ graph/striver/detect_cycle_DG.cpp
@@ -0,0 +1,76 @@
+/******************************************************************* QUESTION ****************************************************************************************/
+
+/*
+Given a Directed Graph with V vertices (Numbered from 0 to V-1) and E edges, check whether it contains any cycle or not.
+
+
+Example 1:
+
+Input:
+
+
+
+Output: 1
+Explanation: 3 -> 3 is a cycle
+
+Example 2:
+
+Input:
+
+
+Output: 0
+Explanation: no cycle in the graph
+
+Your task:
+You dont need to read input or print anything. Your task is to complete the function isCyclic() which takes the integer V denoting the number of vertices 
+and adjacency list as input parameters and returns a boolean value denoting if the given directed graph contains a cycle or not.
+
+
+Expected Time Complexity: O(V + E)
+Expected Auxiliary Space: O(V)
+
+
+Constraints:
+1 ≤ V, E ≤ 105
+*/
+
+/****************************************************************************** ANSWER *****************************************************************************/
+
+class Solution {
+  public:
+    bool check(int node, vector<int> adj[], vector<int>& vis, vector<int>& dfsvis) {
+        vis[node] = 1;
+        dfsvis[node] = 1;
+        
+        for(auto it : adj[node]) {
+            if(!vis[it]) {
+                if(check(it, adj, vis, dfsvis)) {
+                    return true;
+                }
+            }
+            else if(dfsvis[it]) {
+                return true;
+            }
+        }
+        dfsvis[node] = 0;
+        return false;
+    }
+  
+  
+    // Function to detect cycle in a directed graph.
+    bool isCyclic(int V, vector<int> adj[]) {
+        // code here
+        vector<int> vis(V, 0);
+        vector<int> dfsvis(V, 0);
+        
+        for(int i = 0; i < V; i++)  {
+            if(!vis[i]) {
+                if(check(i, adj, vis, dfsvis)) {
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+};
