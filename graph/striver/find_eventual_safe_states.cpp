99bc0d95d9c8f8edfc6ec8df6ae8b9c05e7c29e1
Kanishka Panwar <92727668+kanishka-p1@users.noreply.github.com>
2022-12-26 18:01:56 +0530

    Update find_eventual_safe_states.cpp

--- graph/striver/find_eventual_safe_states.cpp
+++ graph/striver/find_eventual_safe_states.cpp
@@ -87,3 +87,43 @@ public:
 
 /***************************************************************************** BFS **********************************************************************************/
 
+class Solution {
+public:
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int v = graph.size();
+        vector<int> indegree(v, 0);
+        vector<int> revgraph[v];
+        //i -> it
+        // it -> i
+        for(int i = 0; i < v; i++) {
+            for(auto it : graph[i]) {
+                revgraph[it].push_back(i);
+                indegree[i]++;
+            }
+        }
+
+        queue<int> q;
+        vector<int> ans;
+        for(int i = 0; i < v; i++) {
+            if(indegree[i] == 0) {
+                q.push(i);
+            }
+        }
+
+        while(!q.empty()) {
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+
+            for(auto it : revgraph[node]) {
+                indegree[it]--;
+                if(indegree[it] == 0) {
+                    q.push(it);
+                }
+            }
+        }
+
+        sort(ans.begin(), ans.end());
+        return ans;
+    }
+};
