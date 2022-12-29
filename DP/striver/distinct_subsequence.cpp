e41851299ae4208a9608408d1e429480851cc55c
Kanishka Panwar <92727668+kanishka-p1@users.noreply.github.com>
2022-12-29 19:49:10 +0530

    Update distinct_subsequence.cpp

--- DP/striver/distinct_subsequence.cpp
+++ DP/striver/distinct_subsequence.cpp
@@ -124,7 +124,7 @@ public:
     }
 };
 
-/********************************************************************* SPACE OPTIMIZATION **************************************************************************/
+/*************************************************************** SPACE OPTIMIZATION USING 2 ARRAYS *******************************************************************/
 
 class Solution {
 public:
@@ -148,5 +148,28 @@ public:
         }
 
         return prev[n];
+    }
+};
+
+/************************************************************ SPACE OPTIMIZATION USING 1 ARRAY ***********************************************************************/
+
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int m = s.length();
+        int n = t.length();
+        vector<double> prev(n + 1, 0);
+
+        prev[0] = 1;
+
+        for(int i = 1; i <= m; i++) {
+            for(int j = n; j >= 1; j--) {
+                if(s[i - 1] == t[j - 1]) {
+                    prev[j] = prev[j - 1] + prev[j];
+                }
+            }
+        }
+
+        return (int)prev[n];
     }
 };
