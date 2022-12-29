eca319fe9cb809209f62da2750dc17019d6b12cc
Kanishka Panwar <92727668+kanishka-p1@users.noreply.github.com>
2022-12-29 19:40:22 +0530

    Update distinct_subsequence.cpp

--- DP/striver/distinct_subsequence.cpp
+++ DP/striver/distinct_subsequence.cpp
@@ -123,3 +123,30 @@ public:
         return (int)dp[m][n];
     }
 };
+
+/********************************************************************* SPACE OPTIMIZATION **************************************************************************/
+
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int m = s.length();
+        int n = t.length();
+        vector<double> prev(n + 1, 0), curr(n + 1, 0);
+
+        prev[0] = curr[0] = 1;
+
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(s[i - 1] == t[j - 1]) {
+                    curr[j] = prev[j - 1] + prev[j];
+                }
+                else {
+                    curr[j] = prev[j];
+                }
+            }
+            prev = curr;
+        }
+
+        return prev[n];
+    }
+};
