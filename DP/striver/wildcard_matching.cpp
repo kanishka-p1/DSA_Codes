67c0bcc96e6e0e9d2a9b2e1748fe97f518a91002
Kanishka Panwar <92727668+kanishka-p1@users.noreply.github.com>
2023-01-03 22:51:37 +0530

    Update wildcard_matching.cpp

--- DP/striver/wildcard_matching.cpp
+++ DP/striver/wildcard_matching.cpp
@@ -114,4 +114,37 @@ public:
 
 /***************************************************************************** DP ************************************************************************************/
 
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int m = s.size();
+        int n = p.size();
+        vector<vector<bool>> dp(n + 1, vector<bool>(m + 1, false));
+        dp[0][0] = true;
+        for(int i = 1; i <= n; i++) {
+            int flag = true;
+            for(int k = 1; k <= i; k++) {
+                if(p[k - 1] != '*') {
+                    flag = false;
+                    break;
+                }
+            }
+            dp[i][0] = flag;
+        }
 
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+                if(p[i - 1] == s[j - 1] || p[i - 1] == '?') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                else if(p[i - 1] == '*') {
+                    dp[i][j] = dp[i - 1][j] | dp[i][j - 1];
+                }
+                else {
+                    dp[i][j] = false;
+                }
+            }
+        }
+        return dp[n][m];
+    }
+};
