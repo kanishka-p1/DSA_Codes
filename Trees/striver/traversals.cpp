5f504c3bcc25e0dd462c44825e697bb454772750
Kanishka Panwar <92727668+kanishka-p1@users.noreply.github.com>
2023-01-15 17:48:48 +0530

    Create traversals.cpp

---
+++ Trees/striver/traversals.cpp
@@ -0,0 +1,68 @@
+/************************************************************************ PREORDER ***********************************************************************************/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        if(root == NULL) {
+            return {};
+        }
+
+        ans.push_back(root->val);
+        vector<int> left = preorderTraversal(root->left);
+        ans.insert(ans.end(), left.begin(), left.end());
+        vector<int> right = preorderTraversal(root->right);
+        ans.insert(ans.end(), right.begin(), right.end());
+
+        return ans;
+    }
+};
+
+/************************************************************************** INORDER ***********************************************************************************/
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        if(root == NULL) {
+            return {};
+        }
+
+        vector<int> left = inorderTraversal(root->left);
+        vector<int> right = inorderTraversal(root->right);
+
+        left.push_back(root->val);
+        left.insert(left.end(), right.begin(), right.end());
+
+        return left;
+    }
+};
+
+/*************************************************************************** POSTORDER *******************************************************************************/
+
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        if(root == NULL) {
+            return {};
+        }
+
+        vector<int> left = postorderTraversal(root->left);
+        vector<int> right = postorderTraversal(root->right);
+
+        left.insert(left.end(), right.begin(), right.end());
+        left.push_back(root->val);
+
+        return left;
+    }
+};
