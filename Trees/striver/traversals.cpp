4efb6a7029865606b0ff647cece2acabc5a020a1
Kanishka Panwar <92727668+kanishka-p1@users.noreply.github.com>
2023-01-16 01:04:54 +0530

    Update traversals.cpp

--- Trees/striver/traversals.cpp
+++ Trees/striver/traversals.cpp
@@ -29,6 +29,34 @@ public:
     }
 };
 
+// iterative approach
+
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        if(root == NULL) {
+            return {};
+        }
+        stack<TreeNode*> st;
+        st.push(root);
+
+        while(!st.empty()) {
+            root = st.top();
+            st.pop();
+            ans.push_back(root->val);
+            if(root->right != NULL) {
+                st.push(root->right);
+            }
+            if(root->left != NULL) {
+                st.push(root->left);
+            }
+        }
+
+        return ans;
+    }
+};
+
 /************************************************************************** INORDER ***********************************************************************************/
 
 class Solution {
@@ -45,6 +73,35 @@ public:
         left.insert(left.end(), right.begin(), right.end());
 
         return left;
+    }
+};
+
+// iterative approach
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        stack<TreeNode*> st;
+        TreeNode* node = root;
+        
+        while(true) {
+            if(node != NULL) {
+                st.push(node);
+                node = node->left;
+            }
+            else {
+                if(st.empty()) {
+                    break;
+                }
+                node = st.top();
+                st.pop();
+                ans.push_back(node->val);
+                node = node->right;
+            }
+        }
+
+        return ans;
     }
 };
 
