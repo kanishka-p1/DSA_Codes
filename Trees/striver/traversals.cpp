d7f0f587027fdc2d45e2388339bc8cbdb48afc9c
Kanishka Panwar <92727668+kanishka-p1@users.noreply.github.com>
2023-01-16 10:25:30 +0530

    Update traversals.cpp

--- Trees/striver/traversals.cpp
+++ Trees/striver/traversals.cpp
@@ -124,6 +124,79 @@ public:
     }
 };
 
+// postoreder using 2 stacks
+
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        if(root == NULL) {
+            return {};
+        }
+        stack<TreeNode*> st1, st2;
+        st1.push(root);
+
+        while(!st1.empty()) {
+            TreeNode* node = st1.top();
+            st1.pop();
+            st2.push(node);
+            if(node->left != NULL) {
+                st1.push(node->left);
+            }
+            if(node->right != NULL) {
+                st1.push(node->right);
+            }
+        }
+
+        while(!st2.empty()) {
+            ans.push_back(st2.top()->val);
+            st2.pop();
+        }
+
+        return ans;
+    }
+};
+
+// postorder using 1 stack
+
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        TreeNode* curr = root;
+        TreeNode* temp;
+        stack<TreeNode*> st;
+        vector<int> ans;
+        if(root == NULL) {
+            return {};
+        }
+
+        while(curr != NULL || !st.empty()) {
+            if(curr != NULL) {
+                st.push(curr);
+                curr = curr->left;
+            }
+            else {
+                temp = st.top()->right;
+                if(temp == NULL) {
+                    temp = st.top();
+                    st.pop();
+                    ans.push_back(temp->val);
+                    while(!st.empty() && temp == st.top()->right) {
+                        temp = st.top();
+                        st.pop();
+                        ans.push_back(temp->val);
+                    }
+                }
+                else {
+                    curr = temp;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
+
 /*********************************************************************** LEVEL ORDER ********************************************************************************/
 
 class Solution {
