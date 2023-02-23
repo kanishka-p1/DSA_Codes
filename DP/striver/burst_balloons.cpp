ea7f36048195e4b066cbde245924b4d9f85af071
Kanishka Panwar <92727668+kanishka-p1@users.noreply.github.com>
2023-02-24 01:35:30 +0530

    Update burst_balloons.cpp

--- DP/striver/burst_balloons.cpp
+++ DP/striver/burst_balloons.cpp
@@ -32,3 +32,79 @@ n == nums.length
 
 /**************************************************************************** RECURSION ******************************************************************************/
 
+class Solution {
+public:
+    int f(int i, int j, vector<int>& nums) {
+        if(i > j) {
+            return 0;
+        }
+        int ans = INT_MIN;
+        for(int ind = i; ind <= j; ind++) {
+            int cost = nums[i - 1] * nums[ind] * nums[j + 1] + f(i, ind-1, nums) + f(ind+1, j, nums);
+            ans = max(ans, cost);
+        }
+        return ans;
+    }
+
+    int maxCoins(vector<int>& nums) {
+        int n = nums.size();
+        nums.push_back(1);
+        nums.insert(nums.begin(), 1);
+        return f(1, n, nums);
+    }
+};
+
+/************************************************************************ MEMOIZATION *****************************************************************************/
+
+class Solution {
+public:
+    int f(int i, int j, vector<int>& nums, vector<vector<int>>& dp) {
+        if(i > j) {
+            return 0;
+        }
+        if(dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        int ans = INT_MIN;
+        for(int ind = i; ind <= j; ind++) {
+            int cost = nums[i - 1] * nums[ind] * nums[j + 1] + f(i, ind-1, nums, dp) + f(ind+1, j, nums, dp);
+            ans = max(ans, cost);
+        }
+        return dp[i][j] = ans;
+    }
+
+    int maxCoins(vector<int>& nums) {
+        int n = nums.size();
+        nums.push_back(1);
+        nums.insert(nums.begin(), 1);
+        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
+        return f(1, n, nums, dp);
+    }
+};
+
+/***************************************************************************** DP ***********************************************************************************/
+
+class Solution {
+public:
+    int maxCoins(vector<int>& nums) {
+        int n = nums.size();
+        nums.push_back(1);
+        nums.insert(nums.begin(), 1);
+        vector<vector<int>> dp(n+2, vector<int>(n+2, 0));
+
+        for(int i = n; i >= 1; i--) {
+            for(int j = 1; j <= n; j++) {
+                if(i > j) {
+                    continue;
+                }
+                int ans = INT_MIN;
+                for(int ind = i; ind <= j; ind++) {
+                    int cost = nums[i - 1] * nums[ind] * nums[j + 1] + dp[i][ind-1] + dp[ind+1][j];
+                    ans = max(ans, cost);
+                }
+                dp[i][j] = ans;
+            }
+        }
+        return dp[1][n];
+    }
+};
