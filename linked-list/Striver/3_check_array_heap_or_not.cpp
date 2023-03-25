0d2d891d30a39d2589a40330ecbc2bbb2f2181cd
Kanishka Panwar <92727668+kanishka-p1@users.noreply.github.com>
2023-03-25 14:22:58 +0530

    Update 3_check_array_heap_or_not.cpp

--- linked-list/Striver/3_check_array_heap_or_not.cpp
+++ linked-list/Striver/3_check_array_heap_or_not.cpp
@@ -28,7 +28,8 @@ The tree doesn't follows max-heap property 9 is
 smaller than 15 and 10, and 10 is smaller than 11. 
  
 Your Task:  
-You don't need to read input or print anything. Your task is to complete the function isMaxHeap() which takes the array A[] and its size N as inputs and returns  "1", else print "0" (without quotes).
+You don't need to read input or print anything. Your task is to complete the function isMaxHeap() which takes the array A[] and its size N as inputs and 
+returns  "1", else print "0" (without quotes).
 
 Expected Time Complexity: O(N)
 Expected Auxiliary Space: O(1)
@@ -74,6 +75,7 @@ class Solution{
     bool isMaxHeap(int a[], int n)
     {
         // Your code goes here
+           // checking for the childrem element
         for(int i = 0; i < n; i++) {
             if(a[i] > a[2*i + 1] && a[i] > a[2*i + 2]) {
                 continue;
