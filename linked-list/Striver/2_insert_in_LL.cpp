55092f609f7080b3605ddb7a13bc2a07c9579eea
Kanishka Panwar <92727668+kanishka-p1@users.noreply.github.com>
2023-03-25 15:11:17 +0530

    Update 2_insert_in_LL.cpp

--- linked-list/Striver/2_insert_in_LL.cpp
+++ linked-list/Striver/2_insert_in_LL.cpp
@@ -46,3 +46,45 @@ Constraints:
 
 /********************************************************************************** ANSWER **************************************************************************/
 
+// } Driver Code Ends
+/*Structure of the linked list node is as
+struct Node {
+  int data;
+  struct Node * next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+}; */
+
+class Solution{
+  public:
+    //Function to insert a node at the beginning of the linked list.
+    Node *insertAtBegining(Node *head, int x) {
+       // Your code here
+       Node* root = new Node(x);
+       root -> next = head;
+       return root;
+    }
+    
+    
+    //Function to insert a node at the end of the linked list.
+    Node *insertAtEnd(Node *head, int x)  {
+       // Your code here
+       Node* root = new Node(x);
+       
+       if(head == NULL) {
+           return root;
+       }
+       
+       Node* temp = head;
+       while(temp -> next != NULL) {
+           temp = temp -> next;
+       }
+       
+       temp -> next = root;
+       root -> next = NULL;
+       
+       return head;
+    }
+};
