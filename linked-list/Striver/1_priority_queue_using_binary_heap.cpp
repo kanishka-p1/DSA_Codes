8cc3e506e71f6d3a13693fc88bb4c592d30c09cd
Kanishka Panwar <92727668+kanishka-p1@users.noreply.github.com>
2023-03-25 10:19:21 +0530

    Create 1_priority_queue_using_binary_heap.cpp

---
+++ linked-list/Striver/1_priority_queue_using_binary_heap.cpp
@@ -0,0 +1,26 @@
+/************************************************************************ QUESTION *************************************************************************************/
+
+/*
+Given a binary heap implementation of Priority Queue. Extract the maximum element from the queue i.e. remove it from the Queue and return it's value. 
+
+Example 1:
+Input: 4 2 8 16 24 2 6 5
+Output: 24
+Priority Queue after extracting maximum: 16 8 6 5 2 2 4
+Example 2:
+Input: 64 12 8 48 5
+Output: 64
+Priority Queue after extracting maximum: 48 12 8 5
+ 
+Your Task:
+Complete the function extractMax() which extracts the maximum element, i.e. remove it from the Queue and return it's value.
+
+Expected Time Complexity: O(logN)
+Expected Space Complexity: O(N)
+
+Constraints:
+1<=N<=500
+*/
+
+/******************************************************************************** ANSWER *******************************************************************************/
+
