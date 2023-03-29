eb26cbe6554f7567327d3a931cb2096abba0a0e8
Kanishka Panwar <92727668+kanishka-p1@users.noreply.github.com>
2023-03-29 11:40:08 +0530

    Create 1_print_one_to_n.cpp

---
+++ Basics/Striver/Learn_basic_recursion/1_print_one_to_n.cpp
@@ -0,0 +1,42 @@
+/************************************************************************* QUESTION *********************************************************************************/
+
+/*
+Print numbers from 1 to N without the help of loops.
+
+Example 1:
+Input:
+N = 10
+Output: 1 2 3 4 5 6 7 8 9 10
+
+Example 2:
+Input:
+N = 5
+Output: 1 2 3 4 5
+ 
+Your Task:
+This is a function problem. You only need to complete the function printNos() that takes N as parameter and prints number from 1 to N recursively. Don't print newline, it will be added by the driver code.
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(N) (Recursive).
+
+Constraints:
+1 <= N <= 1000
+*/
+
+/*************************************************************************** ANSWER **********************************************************************************/
+
+class Solution{
+    public:
+    //Complete this function
+    void printNos(int N)
+    {
+        //Your code here
+        if(N == 0) {
+            return;
+        }
+        else {
+            printNos(N - 1);
+            cout << N << " ";
+        }
+    }
+};
