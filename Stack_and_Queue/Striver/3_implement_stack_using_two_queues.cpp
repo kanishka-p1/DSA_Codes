1094cae5236bae41f6adef6104542b735623fa47
Kanishka Panwar <92727668+kanishka-p1@users.noreply.github.com>
2023-03-29 17:13:55 +0530

    Update 3_implement_stack_using_two_queues.cpp

--- Stack_and_Queue/Striver/3_implement_stack_using_two_queues.cpp
+++ Stack_and_Queue/Striver/3_implement_stack_using_two_queues.cpp
@@ -78,3 +78,36 @@ int QueueStack :: pop()
         }
         return ans;
 }
+
+
+// METHOD 2
+class MyStack {
+public:
+queue<int> q1, q2;
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        q2.push(x);
+        while(!q1.empty()) {
+            q2.push(q1.front());
+            q1.pop();
+        }
+        swap(q1, q2);
+    }
+    
+    int pop() {
+        int top = q1.front();
+        q1.pop();
+        return top;
+    }
+    
+    int top() {
+        return q1.front();
+    }
+    
+    bool empty() {
+        return q1.empty();
+    }
+};
