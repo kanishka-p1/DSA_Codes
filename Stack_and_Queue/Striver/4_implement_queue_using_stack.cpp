9f0f62551597e393ffcfcd244129cf13c817afcf
Kanishka Panwar <92727668+kanishka-p1@users.noreply.github.com>
2023-03-29 22:40:16 +0530

    Update 4_implement_queue_using_stack.cpp

--- Stack_and_Queue/Striver/4_implement_queue_using_stack.cpp
+++ Stack_and_Queue/Striver/4_implement_queue_using_stack.cpp
@@ -39,3 +39,25 @@ Constraints:
 
 /**************************************************************************** ANSWER ******************************************************************************/
 
+class Queue {
+    stack<int> input, output;
+public:
+
+    void enqueue(int x) {
+        while(!input.empty()) {
+            output.push(input.top());
+            input.pop();
+        }
+        input.push(x);
+        while(!output.empty()) {
+            input.push(output.top());
+            output.pop();
+        }
+    }
+
+    int dequeue() {
+        int ans = input.top();
+        input.pop();
+        return ans;
+    }
+};
