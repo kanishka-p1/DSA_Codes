9ffab0e7331576e5efe4312e5993a8748d2c9c2f
Kanishka Panwar <92727668+kanishka-p1@users.noreply.github.com>
2023-04-04 23:52:59 +0530

    Create longest_consequence_subequence.cpp

---
+++ Arrays/Striver/longest_consequence_subequence.cpp
@@ -0,0 +1,48 @@
+/**************************************************************************** QUESTION *******************************************************************************/
+
+/*
+Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+You must write an algorithm that runs in O(n) time.
+
+Example 1:
+Input: nums = [100,4,200,1,3,2]
+Output: 4
+Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+Example 2:
+Input: nums = [0,3,7,2,5,8,4,6,0,1]
+Output: 9
+
+Constraints:
+0 <= nums.length <= 105
+-109 <= nums[i] <= 109
+*/
+
+/*************************************************************************** ANSWER **********************************************************************************/
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 0) {
+            return 0;
+        }
+        int longest = 0;
+        unordered_set<int> st;
+        for(int i = 0; i < n; i++) {
+            st.insert(nums[i]);
+        }
+        for(auto it : st) {
+            if(st.find(it - 1) == st.end()) {
+                int cnt = 1;
+                int x = it;
+                while(st.find(x+1) != st.end()) {
+                    x = x+1;
+                    cnt = cnt+1;
+                }
+                longest = max(longest, cnt);
+            }
+        }
+
+        return longest;
+    }
+};
