a85c440ab6f5eea6d98420ed97a3a883a3073d4d
Kanishka Panwar <92727668+kanishka-p1@users.noreply.github.com>
2023-04-06 19:48:09 +0530

    Create combination_sum.cpp

---
+++ Recursion/Striver/combination_sum.cpp
@@ -0,0 +1,56 @@
+/***************************************************************************** QUESTION ********************************************************************************/
+
+/*
+Given an array of distinct integers candidates and a target integer target, return a list of all unique combinations of candidates where the chosen numbers sum to 
+target. You may return the combinations in any order. The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the 
+frequency of at least one of the chosen numbers is different.
+The test cases are generated such that the number of unique combinations that sum up to target is less than 150 combinations for the given input.
+
+Example 1:
+Input: candidates = [2,3,6,7], target = 7
+Output: [[2,2,3],[7]]
+Explanation:
+2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
+7 is a candidate, and 7 = 7.
+These are the only two combinations.
+Example 2:
+Input: candidates = [2,3,5], target = 8
+Output: [[2,2,2,2],[2,3,3],[3,5]]
+Example 3:
+Input: candidates = [2], target = 1
+Output: []
+ 
+Constraints:
+1 <= candidates.length <= 30
+2 <= candidates[i] <= 40
+All elements of candidates are distinct.
+1 <= target <= 40
+*/
+
+/************************************************************************** ANSWER *************************************************************************************/
+
+class Solution {
+public:
+    
+    void combination(int x, int target, vector<int> &arr, vector<vector<int>> &res, vector<int> &ans){
+        if(x == arr.size()){   //if it checked for all indexes
+            if(target == 0){      //if target is found
+                res.push_back(ans);
+            }
+            return;
+        }
+        if(arr[x] <= target){   //if we pick the element    
+            ans.push_back(arr[x]);   
+            combination(x, target - arr[x], arr, res, ans);
+            ans.pop_back();   //pop the extra element on returning 
+        }
+        combination(x + 1, target, arr, res, ans); //element not picked
+    }
+    
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> res;
+        vector<int> ans;
+        combination(0, target, candidates, res, ans);
+        return res;
+    }
+};
