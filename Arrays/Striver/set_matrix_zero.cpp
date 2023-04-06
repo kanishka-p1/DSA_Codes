ccbaeb2fc8830e81e96c6eea76d0ca7d64ed9d5a
Kanishka Panwar <92727668+kanishka-p1@users.noreply.github.com>
2023-04-06 13:41:17 +0530

    Update set_matrix_zero.cpp

--- Arrays/Striver/set_matrix_zero.cpp
+++ Arrays/Striver/set_matrix_zero.cpp
@@ -20,6 +20,8 @@ n == matrix[0].length
 
 /******************************************************************************* ANSWER ********************************************************************************/
 
+// METHOD 1
+
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
@@ -41,5 +43,88 @@ public:
                 }
             }
         }
+    }
+};
+
+
+// METHOD 2
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int r = matrix.size();
+        int c = matrix[0].size();
+        vector<int> row(r, 0);
+        vector<int> col(c, 0);
+
+        for(int i = 0; i < r; i++) {
+            for(int j = 0; j < c; j++) {
+                if(matrix[i][j] == 0) {
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
+
+        for(int i = 0; i < r; i++) {
+            for(int j = 0; j < c; j++) {
+                if(row[i] || col[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+};
+
+// METHOD 3 - FOR THE ELEMENTS 1 AND 0 ONLY STRIVER OPTIMAL APPROACH
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int r = matrix.size();
+        int c = matrix[0].size();
+        // vector<int> row(r, 0); -> matrix[..][0]
+        // vector<int> col(c, 0); -> matrix[0][..]
+        int col0 = 1;
+
+        for(int i = 0; i < r; i++) {
+            for(int j = 0; j < c; j++) {
+                if(matrix[i][j] == 0) {
+                    // mark thr ith row
+                    matrix[i][0] = 0;
+                    // mark the jth col
+                    if(j != 0) {
+                        matrix[0][j] = 0;
+                    }
+                    else {
+                        col0 = 0;
+                    }
+                }
+            }
+        }
+
+        for(int i = 1; i < r; i++) {
+            for(int j = 1; j < c; j++) {
+                if(matrix[i][j] != 0) {
+                    // check for row and col
+                    if(matrix[0][j] == 0 || matrix[i][0] == 0) {
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+        }
+
+        if(matrix[0][0] == 0) {
+            for(int j = 0; j < c; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        if(col0 = 0) {
+            for(int i = 0; i < r; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+
     }
 };
