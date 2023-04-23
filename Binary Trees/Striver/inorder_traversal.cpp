6cb051b98909aaa471eeee751c59014650811b49
Kanishka Panwar <92727668+kanishka-p1@users.noreply.github.com>
2023-04-23 15:03:51 +0530

    Update inorder_traversal.cpp

--- Binary Trees/Striver/inorder_traversal.cpp
+++ Binary Trees/Striver/inorder_traversal.cpp
@@ -63,3 +63,32 @@ class Solution {
         return ans;
     }
 };
+
+// ITERATIVE
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        stack<TreeNode*> st;
+        TreeNode* node = root;
+        
+        while(true) {
+            if(node != NULL) {
+                st.push(node);
+                node = node->left;
+            }
+            else {
+                if(st.empty()) {
+                    break;
+                }
+                node = st.top();
+                st.pop();
+                ans.push_back(node->val);
+                node = node->right;
+            }
+        }
+
+        return ans;
+    }
+};
