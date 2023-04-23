784b4d4f9049af0fe9f6614d02089c6ba663b52b
Kanishka Panwar <92727668+kanishka-p1@users.noreply.github.com>
2023-04-23 16:42:57 +0530

    Update postorder_traversal.cpp

--- Binary Trees/Striver/postorder_traversal.cpp
+++ Binary Trees/Striver/postorder_traversal.cpp
@@ -66,3 +66,76 @@ vector <int> postOrder(Node* root)
   
   return ans;
 }
+
+// ITERATIVE USING 2 STACKS
+
+vector <int> postOrder(Node* root)
+{
+  // Your code here
+  vector<int> postOrder;
+  if(root == NULL) {
+      return {};
+  }
+  stack<Node*> st1;
+  stack<Node*> st2;
+  st1.push(root);
+  
+  while(!st1.empty()) {
+      Node* node = st1.top();
+      st1.pop();
+      st2.push(node);
+      if(node->left) {
+          st1.push(node->left);
+      }
+      if(node->right) {
+          st1.push(node->right);
+      }
+  }
+  
+  while(!st2.empty()) {
+      postOrder.push_back(st2.top()->data);
+      st2.pop();
+  }
+  
+  return postOrder;
+}
+
+// ITERATIVE USING 1 STACK
+
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        TreeNode* curr = root;
+        TreeNode* temp;
+        stack<TreeNode*> st;
+        vector<int> ans;
+        if(root == NULL) {
+            return {};
+        }
+
+        while(curr != NULL || !st.empty()) {
+            if(curr != NULL) {
+                st.push(curr);
+                curr = curr->left;
+            }
+            else {
+                temp = st.top()->right;
+                if(temp == NULL) {
+                    temp = st.top();
+                    st.pop();
+                    ans.push_back(temp->val);
+                    while(!st.empty() && temp == st.top()->right) {
+                        temp = st.top();
+                        st.pop();
+                        ans.push_back(temp->val);
+                    }
+                }
+                else {
+                    curr = temp;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
