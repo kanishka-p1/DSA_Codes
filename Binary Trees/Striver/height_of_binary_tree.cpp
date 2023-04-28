48c9d33c9676a7e9fcb69358f44f0bdf1640c376
Kanishka Panwar <92727668+kanishka-p1@users.noreply.github.com>
2023-04-28 12:03:19 +0530

    Create height_of_binary_tree.cpp

---
+++ Binary Trees/Striver/height_of_binary_tree.cpp
@@ -0,0 +1,76 @@
+/************************************************************************ QUESTION ************************************************************************************/
+
+/*
+Given the root of a binary tree, return its maximum depth.
+A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Example 1:
+Input: root = [3,9,20,null,null,15,7]
+Output: 3
+Example 2:
+Input: root = [1,null,2]
+Output: 2
+
+Constraints:
+The number of nodes in the tree is in the range [0, 104].
+-100 <= Node.val <= 100
+*/
+
+/***************************************************************************** ANSWER **********************************************************************************/
+
+// RECURSIVE
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root == NULL){
+            return 0;
+        }
+        int ld = maxDepth(root -> left);
+        int rd = maxDepth(root -> right);
+        return max(ld, rd) + 1;
+    }
+};
+
+// ITERATIVE
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        int level = 0;
+        if(root == NULL) {
+            return level;
+        }
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while(!q.empty()) {
+            int size = q.size();
+            level++;
+            for(int i = 0; i < size; i++) {
+                TreeNode* temp = q.front();
+                q.pop();
+                if(temp->left != NULL) {
+                    q.push(temp->left);
+                }
+                if(temp->right != NULL) {
+                    q.push(temp->right);
+                }
+            }
+        }
+
+        return level;
+    }
+};
