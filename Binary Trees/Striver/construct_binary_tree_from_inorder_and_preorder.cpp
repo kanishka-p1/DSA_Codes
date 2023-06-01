c07d6afe83a52ff43c7cdcdcd79c73709917e608
Kanishka Panwar <92727668+kanishka-p1@users.noreply.github.com>
2023-06-01 15:58:24 +0530

    Create construct_binary_tree_from_inorder_and_preorder.cpp

---
+++ Binary Trees/Striver/construct_binary_tree_from_inorder_and_preorder.cpp
@@ -0,0 +1,65 @@
+/********************************************************************** QUESTION **********************************************************************************/
+
+/*
+Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree, 
+construct and return the binary tree.
+
+Example 1:
+Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+Output: [3,9,20,null,null,15,7]
+Example 2:
+Input: preorder = [-1], inorder = [-1]
+Output: [-1]
+ 
+Constraints:
+1 <= preorder.length <= 3000
+inorder.length == preorder.length
+-3000 <= preorder[i], inorder[i] <= 3000
+preorder and inorder consist of unique values.
+Each value of inorder also appears in preorder.
+preorder is guaranteed to be the preorder traversal of the tree.
+inorder is guaranteed to be the inorder traversal of the tree.
+*/
+
+/*********************************************************************** ANSWER ************************************************************************************/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* helper(vector<int>& preorder, int prestart, int preend, vector<int>& inorder, int instart, int inend, map<int, int>& inmap) {
+        if(prestart > preend || instart > inend) {
+            return NULL;
+        }
+
+        TreeNode* root = new TreeNode(preorder[prestart]);
+        int inroot = inmap[root->val];
+        int numsleft = inroot - instart;
+
+        root->left = helper(preorder, prestart+1, prestart+numsleft, inorder, instart, inroot-1, inmap);
+        root->right = helper(preorder, prestart+numsleft+1, preend, inorder, inroot+1, inend, inmap);
+
+        return root;
+    }
+
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        map<int, int> inmap;
+
+        for(int i = 0; i < inorder.size(); i++) {
+            inmap[inorder[i]] = i;
+        }
+
+        TreeNode* root = helper(preorder, 0, preorder.size()-1, inorder, 0, inorder.size()-1, inmap);
+
+        return root;
+    }
+};
