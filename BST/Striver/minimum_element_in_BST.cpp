71c2c1e0c550246bde83a72ad15fda21cc297b42
Kanishka Panwar <92727668+kanishka-p1@users.noreply.github.com>
2023-06-02 16:46:43 +0530

    Create minimum_element_in_BST.cpp

---
+++ BST/Striver/minimum_element_in_BST.cpp
@@ -0,0 +1,46 @@
+/******************************************************************* QUESTION ************************************************************************************/
+
+/*
+Given a Binary Search Tree. The task is to find the minimum valued element in this given BST.
+
+Example 1:
+Input:
+           5
+         /    \
+        4      6
+       /        \
+      3          7
+     /
+    1
+Output: 1
+Example 2:
+Input:
+             9
+              \
+               10
+                \
+                 11
+Output: 9
+Your Task:
+The task is to complete the function minValue() which takes root as the argument and returns the minimum element of BST. If the tree is empty, there is no minimum 
+element, so return -1 in that case.
+
+Expected Time Complexity: O(Height of the BST)
+Expected Auxiliary Space: O(1).
+
+Constraints:
+0 <= N <= 104
+*/
+
+/************************************************************************** ANSWER ******************************************************************************/
+
+int minValue(Node* root) {
+    // Code here
+    if(root == NULL) {
+        return -1;
+    }
+    while(root->left != NULL) {
+        root = root->left;
+    }
+    return root->data;
+}
