8884bd6175bf73cd6985d689466531504bb1c06a
Kanishka Panwar <92727668+kanishka-p1@users.noreply.github.com>
2023-06-04 00:06:53 +0530

    Update two_sum_IV_BST.cpp

--- BST/Striver/two_sum_IV_BST.cpp
+++ BST/Striver/two_sum_IV_BST.cpp
@@ -19,6 +19,8 @@ root is guaranteed to be a valid binary search tree.
 
 /**************************************************************************** ANSWER *****************************************************************************/
 
+// APPROACH 1 USING INORDER
+
 class Solution {
 public:
     void inorder(TreeNode* root, vector<int>& ans) {  
@@ -44,6 +46,77 @@ public:
             }
             else if(inor[i] + inor[j] > k) {
                 j--;
+            }
+        }
+
+        return false;
+    }
+};
+
+//  APPROACH 2 USING BST ITERATOR
+
+class BSTiterator {
+    stack<TreeNode*> st;
+    // reverse = true -> before
+    // reverse = false -> next
+    bool reverse = true;
+
+    void pushAll(TreeNode* root) {
+        while(root) {
+            st.push(root);
+            if(reverse) {
+                root = root->right;
+            }
+            else {
+                root = root->left;
+            }
+        }
+    }
+
+    public:
+    BSTiterator(TreeNode* root, bool isReverse) {
+        reverse = isReverse;
+        pushAll(root);
+    }
+
+    bool hasNext() {
+        return !st.empty();
+    }
+
+    int next() {
+        TreeNode* temp = st.top();
+        st.pop();
+        if(!reverse) {
+            pushAll(temp->right);
+        }
+        else {
+            pushAll(temp->left);
+        }
+        return temp->val;
+    }
+};
+
+class Solution {
+public:
+    bool findTarget(TreeNode* root, int k) {
+        if(!root) {
+            return false;
+        }
+        BSTiterator l(root, false);
+        BSTiterator r(root, true);
+
+        int i = l.next();
+        int j = r.next();
+
+        while(i < j) {
+            if(i + j == k) {
+                return true;
+            }
+            else if(i + j < k) {
+                i = l.next();
+            }
+            else {
+                j = r.next();
             }
         }
 
