cf3a1420dde28face9ae0703e6ad90cc0fc4ac13
Kanishka Panwar <92727668+kanishka-p1@users.noreply.github.com>
2023-06-06 21:39:20 +0530

    Update 8_merge_K_sorted_linked_list.cpp

--- Heaps/Striver/8_merge_K_sorted_linked_list.cpp
+++ Heaps/Striver/8_merge_K_sorted_linked_list.cpp
@@ -88,3 +88,55 @@ class Solution{
            return root;
     }
 };
+
+
+// APPROACH 2
+
+class compare {
+    public:
+    bool operator() (ListNode* a, ListNode* b) {
+        return a->val > b->val;
+    }
+};
+
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode*, vector<ListNode*>, compare> minheap;
+
+        int k = lists.size();
+        if(k == 0) {
+            return NULL;
+        }
+
+        for(int i = 0; i < k; i++) {
+            if(lists[i] != NULL) {
+                minheap.push(lists[i]);
+            }
+        }
+
+        ListNode* head = NULL;
+        ListNode* tail = NULL;
+
+        while(minheap.size() > 0) {
+            ListNode* top = minheap.top();
+            minheap.pop();
+
+            if(top->next) {
+                minheap.push(top->next);
+            }
+
+            if(head == NULL) {
+                head = top;
+                tail = top;
+            }
+            else {
+                tail->next = top;
+                tail = top;
+            }
+        }
+
+        return head;
+
+    }
+};
